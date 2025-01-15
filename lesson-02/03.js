7c7f38d41230d7f99b1f68a5feb6db70bc6a7f74
PavelGr1shchuk <arthurvlasow@gmail.com>
2025-01-15 22:33:58 +0300

    style: remove logs

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -10,17 +10,12 @@ let grade
 
 if (score >= 0 && score < 50) {
     grade = "F";
-    console.log(grade);
 } else if (score >= 50 && score < 70) {
     grade = "D";
-    console.log(grade);
 } else if (score >= 70 && score < 80) {
     grade = "C";
-    console.log(grade);
 } else if (score >= 80 && score < 90) {
     grade = "B";
-    console.log(grade);
 } else if (score >= 90 && score <= 100) {
     grade = "A";
-    console.log(grade);
 }
