bad8afb14074bfee30ec2e6a8f741dead7b58117
PavelGr1shchuk <arthurvlasow@gmail.com>
2025-02-01 00:40:41 +0300

    feat: lesson-05-01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,12 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    const keys = Object.keys(this.resources)
+    if (!keys.includes(resource)) {
+      console.log("Invalid resource");
+    } else {
+      this.resources[resource] += amount;
+    }
+  }
 }
